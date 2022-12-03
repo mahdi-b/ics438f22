70c11232f376bcf35153749b30666916b9542ad1
mahdi-b <mahdi-b@users.noreply.github.com>
2022-12-03 23:49:31 +0000

    published by GitHub Actions

---
+++ schedule/schedule-info.js
@@ -0,0 +1,9 @@
+moreaEventData = [
+  {title: "Text Analytics: Practical Sessions", url: "/ics438f22/modules/text-analytics", start: "2022-11-21", end: "2022-12-8"},
+  {title: "Introduction to Machine Learning", url: "/ics438f22/modules/ml-intro", start: "2022-09-19", end: "2022-09-29"},
+  {title: "Attention and Tranformers", url: "/ics438f22/modules/transformers", start: "2022-10-18", end: "2022-10-25"},
+  {title: "Distributed Computing and Spark", url: "/ics438f22/modules/map-reduce", start: "2022-09-05", end: "2022-09-16"},
+  {title: "Introduction to Machine Learning", url: "/ics438f22/modules/ml-intro-2", start: "2022-09-29", end: "2022-10-07"},
+  {title: "Big Data Formats", url: "/ics438f22/modules/formats", start: "2022-08-29", end: "2022-09-02T23:00"},
+  {title: "Introduction Big Data Analytics", url: "/ics438f22/modules/introduction", start: "2022-08-22", end: "2022-08-26T23:00"}
+]
